d9d955b1c96a61743f6fbf360e138a3adae6e5ac
Oswald <149827898+OGDegenerate@users.noreply.github.com>
2023-11-17 15:21:59 -0500

    added camera movement and high/low jumps

--- Assets/Player_movement.cs
+++ Assets/Player_movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,31 @@ using UnityEngine;
 public class Player_movement : MonoBehaviour
 {
     public float speed;
+    public float maxSpeed;
     public float rotationSpeed;
+    public float jumpHeight;
+    [SerializeField]
+    private float gravityMultiplier;
+    public float jumpButtonGracePeriod;
+
+    [SerializeField]
+    private Transform cameraTransform;
 
     private CharacterController characterController;
-    
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
+    private float _doubleJumpMultiplier = 1f;
+
+    private bool isJumping;
+    private bool isGrounded;
+    private bool _canDoubleJump = false;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
 
@@ -21,17 +40,82 @@ public class Player_movement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        float magnitude = movementDirection.magnitude;
-        magnitude = Mathf.Clamp01(magnitude); 
+        movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
         movementDirection.Normalize();
 
-        characterController.SimpleMove(movementDirection * speed);
+        float gravity = Physics.gravity.y * gravityMultiplier;
+
+        if (isJumping && ySpeed > 0 && Input.GetButton("Jump") == false)
+        {
+            gravity *= 2; 
+        }
+
+        ySpeed += gravity * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            _canDoubleJump = true;
+            lastGroundedTime = Time.time;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpButtonPressedTime = Time.time;
+            }
+        } else{
+            if (Input.GetButtonDown("Jump") && _canDoubleJump) {
+                ySpeed = jumpHeight * _doubleJumpMultiplier;
+                _canDoubleJump = false;
+            }
+        }
+
+        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+
+            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+            {
+                ySpeed = Mathf.Sqrt(jumpHeight * -3 * gravity);
+                jumpButtonPressedTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = Mathf.Lerp(speed, maxSpeed, 5 * Time.deltaTime);
+        }
+        else
+        {
+            speed = 5;
+        }
+
+        Vector3 velocity = movementDirection * speed;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
 
         if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked; 
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
